45fbddf563d3e75cfbbf3cf50855860e8a270c77
Andrée Hansson <peolanha@gmail.com>
2014-10-20 22:20:03 +0200

    Tabs > Spaces

--- eslint-wrapper.js
+++ eslint-wrapper.js
@@ -1,43 +1,43 @@
 /*global require, exports*/
 (function() {
-    "use strict";
+"use strict";
 
-    var CLIEngine = require("./node_modules/eslint/lib/cli-engine");
+	var CLIEngine = require("./node_modules/eslint/lib/cli-engine");
 
-    function lintFile(options) {
-        try {
-            var engine = new CLIEngine(options.options);
-            var result = engine.executeOnFiles(options.files);
-            var messages = result.results.map(function(file) {
-                return {
-                    file: file.file,
-                    messages: file.messages.map(function(message) {
-                        delete message.node;
-                        return message;
-                    })
-                };
-            });
-            return JSON.stringify(messages);
-        } catch (e) {
-            return JSON.stringify(e);
-        }
-    }
+	function lintFile(options) {
+		try {
+			var engine = new CLIEngine(options.options);
+			var result = engine.executeOnFiles(options.files);
+			var messages = result.results.map(function(file) {
+				return {
+					file: file.file,
+					messages: file.messages.map(function(message) {
+						delete message.node;
+						return message;
+					})
+				};
+			});
+			return JSON.stringify(messages);
+		} catch (e) {
+			return JSON.stringify(e);
+		}
+	}
 
-    exports.init = function init(domainManager) {
-        if (!domainManager.hasDomain("ESLintWrapper")) {
-            domainManager.registerDomain("ESLintWrapper", { major: 0, minor: 1 });
-        }
-        domainManager.registerCommand(
-            "ESLintWrapper",
-            "lint",
-            lintFile,
-            false,
-            "Returns lint warnings and errors",
-            [{
-                name: "options",
-                type: "object",
-                description: "Options to invoke ESLint with"
-            }]
-        );
-    };
+	exports.init = function init(domainManager) {
+		if (!domainManager.hasDomain("ESLintWrapper")) {
+			domainManager.registerDomain("ESLintWrapper", { major: 0, minor: 1 });
+		}
+		domainManager.registerCommand(
+			"ESLintWrapper",
+			"lint",
+			lintFile,
+			false,
+			"Returns lint warnings and errors",
+			[{
+				name: "options",
+				type: "object",
+				description: "Options to invoke ESLint with"
+			}]
+		);
+	};
 }());
